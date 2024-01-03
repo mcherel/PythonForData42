2aabb59ab33ca33504d5ab2aee46705f3a4ff505
Maria CHEREL SOROKINA <mcherel-@paul-f4Ar8s2.clusters.42paris.fr>
2024-01-03 14:09:56 +0100

    ex02

--- Python0/ex02/find_ft_type.py
+++ Python0/ex02/find_ft_type.py
@@ -1,24 +1,5 @@
 #!/usr/bin/env python3.10
 
-""" def all_thing_is_obj(obj: any) -> int:
-    try:
-        match obj:
-            case str():
-                print(f"{obj} is in the kitchen : {type(obj)}")
-            case list():
-                print(f"List : {type(obj)}")
-            case tuple():
-                print(f"Tuple : {type(obj)}")
-            case set():
-                print(f"Set : {type(obj)}")
-            case dict():
-                print(f"Dict : {type(obj)}")
-            case _:
-                print("Type not found")
-    except TypeError:
-        exit()
-    return 42 """
-
 def all_thing_is_obj(obj: any=None) -> int:
     if not obj:
         exit()
