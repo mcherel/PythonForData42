97a163594bbd3d77b57b1c6f94a9c0aa63bf804d
Maria CHEREL SOROKINA <mcherel-@paul-f3Br9s1.clusters.42paris.fr>
2024-06-13 10:48:12 +0200

    tests expeted output ok

--- Python0/ex04/whatis.py
+++ Python0/ex04/whatis.py
@@ -3,15 +3,25 @@ import sys
 def whatlist():
     try:
         args = sys.argv
+
+        assert len(args) > 1
         assert len(args) == 2, "more than one argument is provided" # assertion error : user declares a condition to be true
-        #print(args[1].isnumeric())
-        #print(-sys.maxsize-1)
         num = args[1]
         assert num.lstrip('-').isnumeric(), "argument is not an integer"
         num = int(num)
         assert -sys.maxsize-1 <= num <= sys.maxsize, "argument is not an integer"
-        #print(num)
-        #print(type(num))
+
+        response = "I'm Odd" if num % 2 else  "I'm Even"
+        print(response)
+        
     except AssertionError as er:
-        print(f'AssertionError: "{er}"')
-whatlist()
+        if er.args:
+            print(f'AssertionError: "{er}"')
+            
+    print()
+
+def main():
+    whatlist()
+
+if __name__== "__main__":
+    main()
