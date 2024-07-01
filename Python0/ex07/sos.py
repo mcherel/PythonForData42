526239cdddcee60e94e4a8fd7d3154716212a47f
Maria CHEREL SOROKINA <mcherel-@paul-f5Ar1s1.clusters.42paris.fr>
2024-07-01 13:32:04 +0200

    morse converter

---
+++ Python0/ex07/sos.py
@@ -0,0 +1,39 @@
+import sys
+
+
+def arg_check(args) -> bool:
+    try:
+        assert len(args) == 2
+        string = args[1]
+        for s in string.split():
+            assert s.isalnum()
+        return True
+
+    except AssertionError:
+        print('AssertionError: the arguments are bad')
+        sys.exit()
+
+
+def morse_converter(msg: str):
+    morse_code_dict = {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+        'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+        'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+        'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+        'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+        'Z': '--..',
+        '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
+        '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+        ' ': '/'
+    }
+    return [morse_code_dict[i] for i in msg]
+
+
+def main():
+    args = sys.argv
+    if arg_check(args):
+        print(" ".join(morse_converter(str(args[1]).upper())))
+
+
+if __name__ == "__main__":
+    main()
