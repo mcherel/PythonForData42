ee0872e361bef96d35ac2c922521bdd6d9fed77f
Maria CHEREL SOROKINA <mcherel-@paul-f5Ar1s1.clusters.42paris.fr>
2024-07-01 14:44:11 +0200

    new 08

---
+++ Python0/ex08/tester.py
@@ -0,0 +1,16 @@
+from time import sleep
+from tqdm import tqdm
+from Loading import ft_tqdm
+
+for elem in ft_tqdm(range(333)):
+    sleep(0.005)
+print()
+for elem in tqdm(range(333)):
+    sleep(0.005)
+print()
+""" Expected output: (you must have a function as close as possible to the original
+version)
+$> python tester.py
+100%|[===============================================================>]| 333/333
+100%| | 333/333 [00:01<00:00, 191.61it/s]
+16 """
