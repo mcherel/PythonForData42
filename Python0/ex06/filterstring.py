5643e64b2902a01d4a14acf711d555b2b11a9a5e
Maria CHEREL SOROKINA <mcherel-@paul-f5Ar1s1.clusters.42paris.fr>
2024-07-01 10:53:54 +0200

    working program without using ft_filter)

--- Python0/ex06/filterstring.py
+++ Python0/ex06/filterstring.py
@@ -4,24 +4,30 @@ import sys
 def arg_check(args) -> bool:
     try:
         
-        assert len(args) == 3, "the arguments are bad" # assertion error : user declares a condition to be true
+        assert len(args) == 3 # assertion error : user declares a condition to be true
         string = args[1]
         for s in string.split():
-          assert s.isalnum(), "argument is not an alphanumerical character"
+          assert s.isalnum()
         num = args[2]
-        assert num.isnumeric(), "argument is not a positive integer"
-        assert -sys.maxsize-1 <= int(num) <= sys.maxsize, "argument is not an integer"
+        assert num.isnumeric()
+        assert -sys.maxsize-1 <= int(num) <= sys.maxsize
         
         return True
         
     except AssertionError as er:
-        if er.args:
-            print(f'AssertionError: "{er}"')
+            print(f'AssertionError: the arguments are bad')
             sys.exit()
+
+def ft_list(a):
+    return lambda words: [i for i in words if len(i) > a]
 
 def main():
     args = sys.argv
-    print(arg_check(args))
+    if arg_check(args):
+      words = args[1].split()
+      num = int(args[2])
+      ft_checker = ft_list(num)
+      print(ft_checker(words))
 
 if __name__== "__main__":
     main()
