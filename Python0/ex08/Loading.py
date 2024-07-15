a8084c6f565b379633b750c0d715111fdc4bf93f
Maria CHEREL SOROKINA <mcherel-@paul-f4Br1s1.clusters.42paris.fr>
2024-07-15 14:08:49 +0200

    function, flake8 ok

--- Python0/ex08/Loading.py
+++ Python0/ex08/Loading.py
@@ -1,2 +1,30 @@
-def ft_tqdm(lst: range) -> None:
-    print(lst)
+
+import sys
+import shutil
+
+
+def ft_tqdm(lst: range) -> None:  # type: ignore
+    """
+    Mimic the behavior of tqdm progress bar for an iterable.
+
+    Args:
+        lst (range): The iterable (range) to iterate over with a progress bar.
+
+    Yields:
+        int: The items from the iterable `lst`, one at a time.
+    """
+    total = len(lst)
+    for i, item in enumerate(lst, start=1):
+        columns = shutil.get_terminal_size().columns
+        bar_length = columns - 40 if columns > 40 else 10
+        percent = int((i / total) * 100)
+        filled_length = int((i / total) * bar_length) - 3
+        filled_print = '=' * filled_length + ">"
+        not_filled_print = ' ' * (bar_length - filled_length - 3)
+        bar = filled_print + not_filled_print
+        sys.stdout.write(f'\r{percent}%|[{bar}]| {i}/{total}')
+        sys.stdout.flush()
+        yield item
+
+    sys.stdout.write('\n')
+    return None
--- Python0/ex08/tester.py
+++ Python0/ex08/tester.py
@@ -2,15 +2,16 @@ from time import sleep
 from tqdm import tqdm
 from Loading import ft_tqdm
 
+print()
+print('ft_tqdm')
 for elem in ft_tqdm(range(333)):
     sleep(0.005)
 print()
+print('tqdm1')
 for elem in tqdm(range(333)):
     sleep(0.005)
 print()
-""" Expected output: (you must have a function as close as possible to the original
-version)
-$> python tester.py
-100%|[===============================================================>]| 333/333
-100%| | 333/333 [00:01<00:00, 191.61it/s]
-16 """
+print('tqdm2')
+for elem in tqdm(range(333)):
+    sleep(0.005)
+print()
