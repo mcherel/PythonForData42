3aab0aae68fd9ecb31de1fbc01b286da0409ec10
Maria <mashasorokina@hotmail.com>
2024-08-08 13:46:08 +0200

    exOO finished - working on exceptions

--- Python1/ex00/give_bmi.py
+++ Python1/ex00/give_bmi.py
@@ -3,28 +3,75 @@ import numpy as np
 
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
     """
-    Calcule l'IMC (Indice de Masse Corporelle) pour une liste de tailles et de poids donnés.
+    Calculate BMI (Body Mass Index) for a list of given heights and weights.
 
     Args:
-        height (list[int | float]): Liste des tailles en mètres.
-        weight (list[int | float]): Liste des poids en kilogrammes.
+        height (list[int | float]): List of heights in meters.
+        weight (list[int | float]): List of weights in kilograms.
 
     Returns:
-        list[int | float]: Liste des valeurs d'IMC correspondantes.
+        list[float]: List of corresponding BMI values.
+
+    Raises:
+        ValueError: If the lists of heights and weights are not of the same length.
+        TypeError: If the values in the lists are not integers or floats.
     """
-    bmi = np.array(weight) / np.array(height) ** 2
-    return bmi.tolist()
+    try:
+        len_err = "The lists of heights and weights must have the same length."
+        if not len(height) == len(weight):
+            raise ValueError(len_err)
+        type_err = "All values in the lists must be integers or floats."
+        if not all(isinstance(i, (int, float)) for i in height):
+            raise TypeError(type_err)
+        if not all(isinstance(i, (int, float)) for i in weight):
+            raise TypeError(type_err)
+            
+        bmi = np.array(weight) / np.array(height) ** 2
+        return bmi.tolist()
+    except (ValueError, TypeError) as e:
+        print ("Error:", e)
+        exit()
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
     """
-    Applique une limite supérieure aux valeurs d'IMC pour déterminer si elles dépassent cette limite.
+    Apply an upper limit to BMI values to determine if they exceed this limit.
 
     Args:
-        bmi (list[int | float]): Liste des valeurs d'IMC.
-        limit (int): Valeur limite d'IMC.
+        bmi (list[float]): List of BMI values.
+        limit (int): BMI limit value.
 
     Returns:
-        list[bool]: Liste indiquant pour chaque valeur d'IMC si elle dépasse la limite (True) ou non (False).
+        list[bool]: List indicating for each BMI value whether it exceeds the limit (True) or not (False).
+
+    Raises:
+        TypeError: If the value of the limit is not integer.
+        TypeError: If the values in the BMI list are not integers or floats.
     """
-    return (np.array(bmi) > limit).tolist()
+    try:
+        if not isinstance(limit, (int)):
+            raise TypeError("Limit must be an integer")
+        type_err = "All values in the lists must be integers or floats."
+        if not all(isinstance(i, (int, float)) for i in bmi):
+            raise TypeError(type_err)
+        return (np.array(bmi) > limit).tolist()
+    except (TypeError) as e:
+        print ("Error:", e)
+        exit()
+
+
+def main():
+    """
+    Main function to test the give_bmi and apply_limit functions.
+    """
+    heights = [1.75, 1.8, 1.65, 1.1, 1.5, 1.75]
+    weights = [65, 75, 50, 20, 50, 88]
+    bmi = give_bmi(heights, weights)
+    print("Calculated BMI:", bmi)
+
+    limit = 25
+    bmi_above_limit = apply_limit(bmi, limit)
+    print(f"BMI above the limit of {limit}:", bmi_above_limit)
+
+if __name__ == "__main__":
+    main()
--- Python1/ex00/tester.py
+++ Python1/ex00/tester.py
@@ -1,39 +1,13 @@
 from give_bmi import give_bmi, apply_limit
 
-height = [2.71, 1.15]
-weight = [165.3, 38.4]
-bmi = give_bmi(height, weight)
-print(bmi, type(bmi))
-print(apply_limit(bmi, 26))
 
-""" height = ["hello", 1.15]
-weight = [165.3, 38.4]
-bmi = give_bmi(height, weight)
-print(bmi, type(bmi))
-print(apply_limit(bmi, 26)) """
+def main():
+    height = [2.71, 1.15]
+    weight = [165.3, 38.4]
+    bmi = give_bmi(height, weight)
+    print(bmi, type(bmi))
+    print(apply_limit(bmi, 26))
 
 
-
-""" import numpy as np
-
-# random float 0-1
-a = np.random.rand(4,2)
-print(a)
-# random int from -4 to 8
-b = np.random.randint(-4,8, size=(3,3))
-print(b)
-
-c = np.identity(5)
-print(c)
-
-# 11111
-# 10001
-# 10901
-# 10001
-# 11111
-
-output = np.ones((5,5))
-z = np.zeros((3,3))
-z[1,1] = 9
-output[1:4,1:4] = z
-print(output) """
+if __name__ == "__main__":
+    main()
