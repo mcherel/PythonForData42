5c0d42524bba24f6d544696ebe4bc1fc19134e83
Maria CHEREL SOROKINA <mcherel-@paul-f5Ar4s1.clusters.42paris.fr>
2024-08-22 13:47:27 +0200

    parsing

--- Python2/ex00/load_csv.py
+++ Python2/ex00/load_csv.py
@@ -1,13 +1,63 @@
 import csv
 import numpy as np
-def load(path: str) -> list:
-    """"""
-    dataset = []
-    with open (path, 'r', encoding='utf-8-sig') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        stripped_rows = list([[field.strip() for field in row] for row in reader])
-        for row in stripped_rows:
-            dataset.append(row)
+import os
 
-    print(f"Loading dataset of dimensions {np.array(dataset).shape}")
-    return np.array(dataset)
+def is_accepted_format(path: str, formats: list) -> bool:
+    """Checks if the format is in the given list of formats.
+    Returns: bool
+    """
+    _, ext = os.path.splitext(path)
+    return (ext.lower() in formats)
+
+
+def load(file_path: str) -> np.array:
+    """Loads csv file counts data lines
+        Return: whole array including the header"""
+    if not os.path.exists(file_path):
+        print(f"Error: No such file or directory: '{file_path}'")
+        exit()
+    # if the name is the same as extension
+    if file_path == ".csv":
+        print("Error: wrong file name")
+        exit()
+    # if the format of the image is not supported
+    if not is_accepted_format(file_path, [".csv"]):
+        print("Error: wrong extention")
+        exit()
+    with open (file_path, 'r', encoding='utf-8-sig') as csvfile:
+        reader = list(csv.reader(csvfile, delimiter=','))
+        if not reader:
+            print("Error: the file is empty")
+            exit()
+        if len(reader) < 2 or len(reader[0]) < 2:
+            print("Error: there is no enough data in the file (min 2 x 2)")
+            exit()
+        len_err = "All lists in file must have the same length."
+        if not all(len(reader[0]) == len(i) for i in reader):
+            print(f"Error: {len_err}")
+            exit()
+        print(len(reader), len(reader[0]) )
+        stripped_rows = list([[field.strip() for field in row] for row in reader[1:]])
+        print(f"Loading dataset of dimensions {np.array(stripped_rows).shape}")
+        return np.array(reader)
+
+def main():
+    load("")
+    print(load("five.csv"))
+    print("later")
+    print(load("four.csv"))
+    load("one.csv")
+    load("empty.csv")
+    load(".csv")
+    load("hello.txt")
+    """ X = []
+    with open(file="../csv_files/life_expectancy_years.csv", mode='r', encoding='utf-8') as csvfile:
+        dataset = csv.reader(csvfile)
+        next(dataset)
+        for first in dataset:
+            X.append(first)
+    print(np.array(X)) """
+
+
+if __name__ == "__main__":
+    main()
