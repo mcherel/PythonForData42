8b158bf4d7da843530148a45f66579b2410bd8db
Maria CHEREL SOROKINA <mcherel-@paul-f5Ar4s1.clusters.42paris.fr>
2024-08-22 14:06:08 +0200

    flake8

--- Python2/ex00/load_csv.py
+++ Python2/ex00/load_csv.py
@@ -2,6 +2,7 @@ import csv
 import numpy as np
 import os
 
+
 def is_accepted_format(path: str, formats: list) -> bool:
     """Checks if the format is in the given list of formats.
     Returns: bool
@@ -24,22 +25,27 @@ def load(file_path: str) -> np.array:
     if not is_accepted_format(file_path, [".csv"]):
         print("Error: wrong extention")
         exit()
-    with open (file_path, 'r', encoding='utf-8-sig') as csvfile:
+    with open(file_path, 'r', encoding='utf-8-sig') as csvfile:
         reader = list(csv.reader(csvfile, delimiter=','))
+        # if cannot genetate a data table form the given file
         if not reader:
             print("Error: the file is empty")
             exit()
+        # if the data table is too small
         if len(reader) < 2 or len(reader[0]) < 2:
             print("Error: there is no enough data in the file (min 2 x 2)")
             exit()
         len_err = "All lists in file must have the same length."
+        # if the data table is not rectanngular
         if not all(len(reader[0]) == len(i) for i in reader):
             print(f"Error: {len_err}")
             exit()
-        print(len(reader), len(reader[0]) )
-        stripped_rows = list([[field.strip() for field in row] for row in reader[1:]])
+        stripped_rows = list([[field.strip()
+                             for field in row]
+                             for row in reader[1:]])
         print(f"Loading dataset of dimensions {np.array(stripped_rows).shape}")
         return np.array(reader)
+
 
 def main():
     load("")
@@ -50,13 +56,6 @@ def main():
     load("empty.csv")
     load(".csv")
     load("hello.txt")
-    """ X = []
-    with open(file="../csv_files/life_expectancy_years.csv", mode='r', encoding='utf-8') as csvfile:
-        dataset = csv.reader(csvfile)
-        next(dataset)
-        for first in dataset:
-            X.append(first)
-    print(np.array(X)) """
 
 
 if __name__ == "__main__":
