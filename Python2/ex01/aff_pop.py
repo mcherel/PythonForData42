dc018a2a23da2e541d9f57c5e14a4354c4395041
Maria <mashasorokina@hotmail.com>
2024-08-27 16:37:12 +0200

    ex02

---
+++ Python2/ex01/aff_pop.py
@@ -0,0 +1,80 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import load_csv
+
+
+
+def main():
+    file = "../csv_files/population_total.csv"
+    lines = load_csv.load(file)
+    x = []
+    y = []
+    y1 = []
+    end_year = 2040
+    countries = [i[0] for i in lines[1:]]
+    chosen_countries = ['France', 'Russia']
+    countries_idx = []
+    try:
+        for country in chosen_countries:
+                countries_idx.append(countries.index(country) + 1)
+    except ValueError:
+        print(f"{country} not found in the list of countries.")
+        return
+    """ stripped_rows = list([[field.strip()
+                             for field in row]
+                             for row in reader[1:]]) """
+    for year in lines[0][1:]:
+          if int(year) <= end_year:
+              x.append(int(year.strip()))
+    # France's life expectancy
+    for i, value in enumerate(lines[countries_idx[0]][1:]):
+          year = int(lines[0][i + 1])  # Get the corresponding year from the CSV header
+          if year <= end_year:
+              y.append(float(value.replace("M", "").strip()))
+     # Russia's life expectancy
+    for i, value in enumerate(lines[countries_idx[1]][1:]):
+          year = int(lines[0][i + 1])  # Get the corresponding year from the CSV header
+          if year <= end_year:
+              y1.append(float(value.replace("M", "").strip()))
+    #print(len(x))
+    #print(len(y))
+    print(x)
+    print(y)
+
+    # is data loaded correctly
+    if len(x) != len(y):
+         print("Data length mismach between years and life expectancy")
+    # crop data to year 2040
+    cropped_x = [year for year in x if year <= end_year]
+    cropped_y = [y[i] for i in range(len(y)) if x[i] <= end_year] + [y1[i] for i in range(len(y)) if x[i] <= end_year]
+
+    # plotting
+    plt.plot(x, y, color='g', label='Population France')
+    plt.plot(x, y1, color='b', label='Population Russia')
+    plt.title('Population Projections')
+    # plt.xticks(rotation=25)
+    plt.xlabel('Year') # adds a label to the x-axis
+    plt.ylabel('Population') # adds a label to the y-axis.
+
+    # dynamic margin
+    y_margin = (max(cropped_y) - min(cropped_y)) * 0.1
+    y_min = min(cropped_y) - y_margin
+    y_max = max(cropped_y) + y_margin
+
+    plt.margins(x=0.05, y=0.05)
+    plt.xticks(np.arange(int(min(cropped_x) // 10 * 10), max(cropped_x) + 1, 40))
+    plt.yticks(np.arange(int(min(cropped_y) // 10 * 10), int(max(cropped_y)), 20))
+    #plt.grid(True)
+    #plt.xlim(min(x), max(x))
+    plt.ylim(y_min, y_max)
+    #plt.xticks(x)
+
+    # Graph printing
+    plt.legend() # ensures the labels are displayed.
+    
+    # Show plot
+    plt.show()
+
+
+if __name__ == "__main__":
+      main()
---
+++ Python2/ex02/load_csv.py
@@ -0,0 +1,62 @@
+import csv
+import numpy as np
+import os
+
+
+def is_accepted_format(path: str, formats: list) -> bool:
+    """Checks if the format is in the given list of formats.
+    Returns: bool
+    """
+    _, ext = os.path.splitext(path)
+    return (ext.lower() in formats)
+
+
+def load(file_path: str) -> np.array:
+    """Loads csv file counts data lines
+        Return: whole array including the header"""
+    if not os.path.exists(file_path):
+        print(f"Error: No such file or directory: '{file_path}'")
+        exit()
+    # if the name is the same as extension
+    if file_path == ".csv":
+        print("Error: wrong file name")
+        exit()
+    # if the format of the image is not supported
+    if not is_accepted_format(file_path, [".csv"]):
+        print("Error: wrong extention")
+        exit()
+    with open(file_path, 'r', encoding='utf-8-sig') as csvfile:
+        reader = list(csv.reader(csvfile, delimiter=','))
+        # if cannot genetate a data table form the given file
+        if not reader:
+            print("Error: the file is empty")
+            exit()
+        # if the data table is too small
+        if len(reader) < 2 or len(reader[0]) < 2:
+            print("Error: there is no enough data in the file (min 2 x 2)")
+            exit()
+        len_err = "All lists in file must have the same length."
+        # if the data table is not rectanngular
+        if not all(len(reader[0]) == len(i) for i in reader):
+            print(f"Error: {len_err}")
+            exit()
+        stripped_rows = list([[field.strip()
+                             for field in row]
+                             for row in reader[1:]])
+        print(f"Loading dataset of dimensions {np.array(stripped_rows).shape}")
+        return np.array(reader)
+
+
+def main():
+    load("")
+    print(load("five.csv"))
+    print("later")
+    print(load("four.csv"))
+    load("one.csv")
+    load("empty.csv")
+    load(".csv")
+    load("hello.txt")
+
+
+if __name__ == "__main__":
+    main()
