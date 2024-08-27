de817a5ff5c2059b9063a15d65decc5cba600959
Maria <mashasorokina@hotmail.com>
2024-08-27 14:58:54 +0200

    working version

--- Python2/ex01/aff_life.py
+++ Python2/ex01/aff_life.py
@@ -10,19 +10,28 @@ def main():
     x = []
     y = []
     countries = [ i[0] for i in lines[1:]]
-    france_idx = countries.index('France')
-
+    chosen_countries = ['France']
+    countries_idx = []
+    try:
+        for country in chosen_countries:
+                countries_idx.append(countries.index(country) + 1)
+    except ValueError:
+        print(f"{country} not found in the list of countries.")
+        return
     """ stripped_rows = list([[field.strip()
                              for field in row]
                              for row in reader[1:]]) """
     for year in lines[0][1:]:
          x.append(int(year.strip()))
-    for value in lines[france_idx][1:]:
+    # France's life expectancy
+    for value in lines[countries_idx[0]][1:]:
          y.append(float(value.strip()))
     #print(len(x))
     #print(len(y))
-    #print(x)
-    #print(y)
+    print(x)
+    print(y)
+
+    # is data loaded correctly
     if len(x) != len(y):
          print("Data length mismach between years and life expectancy")
 
@@ -32,56 +41,19 @@ def main():
     # plt.xticks(rotation=25)
     plt.xlabel('Year') # adds a label to the x-axis
     plt.ylabel('Life expectancy') # adds a label to the y-axis.
+
+    # dynamic margin
+    y_margin = (max(y) - min(y)) * 0.1
+    y_min = min(y) - y_margin
+    y_max = max(y) + y_margin
+
+    plt.margins(x=0.05, y=0.05)
+    plt.xticks(np.arange(min(x), max(x) + 1, 40))
+    plt.yticks(np.arange(int(min(y) // 10 * 10), int(max(y) + 20), 10))
     #plt.grid(True)
-    plt.xlim(min(x), max(x))
-    plt.ylim(min(y), max(y))
+    #plt.xlim(min(x), max(x))
+    plt.ylim(y_min, y_max)
     #plt.xticks(x)
-    """ fig, ax = plt.subplots()
-    ax.set_xticks(np.arange(0, max(x), 50))
-    ax.set_yticks(np.arange(0, max(y), 50)) """
-    """     # Check if the ddata file exists
-    if os.path.exists(file):
-            # Loading  data into two lists
-            data = pd.read_csv(file)
-            X = data['km'].values.reshape(-1, 1)
-            y = data['price'].values.reshape(-1, 1)
-
-            #Plot the data
-            plt.scatter(X, y, label='Car prices')
-            plt.xlabel('Mileage (km)') # adds a label to the x-axis
-            plt.ylabel('Price ($)') # adds a label to the y-axis.
-            plt.title('Car Price Prediction') # adds a title to the plot.
-
-    else:
-            print(f"There is no {file}")
-            if os.path.exists(training_data_file):
-                os.remove(training_data_file)
-            exit()
-
-    #Plot regression line from training data if available
-    if os.path.exists(training_data_file):
-            theta0, theta1 = open_csv.open_csv(training_data_file)
-
-            # Adding a red line
-            xmin, xmax = X.min(), X.max()
-            ordonne = np.linspace(xmin, xmax)
-            plt.plot(ordonne, theta1 * ordonne + theta0, color='r', label='Linear Regression Line')
-
-            # Prediction
-            y_predict = estimate_price.hypothesis(theta0, theta1, X)
-
-            # Print Mean Absolute Error (MAE)
-            print(f'MAE: \t{metrics.mean_absolute_error(y, y_predict)}')
-
-            # Print Mean Squared Error (MSE)
-            print(f'MSE: \t{metrics.mean_squared_error(y, y_predict)}')
-
-            # Print Root Mean Squared Error (RMSE)
-            print(f'RMSE: \t{np.sqrt(metrics.mean_squared_error(y, y_predict))}')
-
-            # Print R-squared (R²)
-            print(f'R²: \t{metrics.r2_score(y, y_predict)}') """
-
 
     # Graph printing
     plt.legend() # ensures the labels are displayed.
