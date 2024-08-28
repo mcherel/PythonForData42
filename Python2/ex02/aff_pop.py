32e0eddc6f0820f0a3f19825f3576d2123bf2382
Maria CHEREL SOROKINA <mcherel-@made-f0Ar1s9.clusters.42paris.fr>
2024-08-28 14:39:21 +0200

    flake-OK

--- Python2/ex02/aff_pop.py
+++ Python2/ex02/aff_pop.py
@@ -3,7 +3,6 @@ import numpy as np
 import load_csv
 
 
-
 def main():
     file = "../csv_files/population_total.csv"
     lines = load_csv.load(file)
@@ -16,46 +15,39 @@ def main():
     countries_idx = []
     try:
         for country in chosen_countries:
-                countries_idx.append(countries.index(country) + 1)
+            countries_idx.append(countries.index(country) + 1)
     except ValueError:
         print(f"{country} not found in the list of countries.")
         return
-    """ stripped_rows = list([[field.strip()
-                             for field in row]
-                             for row in reader[1:]]) """
     for year in lines[0][1:]:
-          if int(year) <= end_year:
-              x.append(int(year.strip()))
+        if int(year) <= end_year:
+            x.append(int(year.strip()))
     # France's life expectancy
     for i, value in enumerate(lines[countries_idx[0]][1:]):
-          year = int(lines[0][i + 1])  # Get the corresponding year from the CSV header
-          if year <= end_year:
-              y.append(float(value.replace("M", "").strip()))
-     # Russia's life expectancy
+        year = int(lines[0][i + 1])
+        if year <= end_year:
+            y.append(float(value.replace("M", "").strip()))
+    # Russia's life expectancy
     for i, value in enumerate(lines[countries_idx[1]][1:]):
-          year = int(lines[0][i + 1])  # Get the corresponding year from the CSV header
-          if year <= end_year:
-              y1.append(float(value.replace("M", "").strip()))
-    #print(len(x))
-    #print(len(y))
-    print(x)
-    print(y)
+        year = int(lines[0][i + 1])
+        if year <= end_year:
+            y1.append(float(value.replace("M", "").strip()))
 
     # is data loaded correctly
     if len(x) != len(y):
-         print("Data length mismach between years and life expectancy")
+        print("Data length mismach between years and life expectancy")
     # crop data to year 2040
     cropped_x = [year for year in x if year <= end_year]
-    cropped_y = [y[i] for i in range(len(y)) if x[i] <= end_year] + [y1[i] for i in range(len(y)) if x[i] <= end_year]
+    cropped_y = [y[i] for i in range(len(y)) if x[i] <= end_year]
+    cropped_y.extend([y1[i] for i in range(len(y)) if x[i] <= end_year])
 
     # plotting
+    plt.title('Population Projections')
+    plt.xlabel('Year')  # adds a label to the x-axis
+    plt.ylabel('Population')  # adds a label to the y-axis.
     plt.plot(x, y, color='g', label='Population France')
     plt.plot(x, y1, color='b', label='Population Russia')
     plt.legend(loc='lower right')
-    plt.title('Population Projections')
-    # plt.xticks(rotation=25)
-    plt.xlabel('Year') # adds a label to the x-axis
-    plt.ylabel('Population') # adds a label to the y-axis.
 
     # dynamic margin
     y_margin = (max(cropped_y) - min(cropped_y)) * 0.1
@@ -63,19 +55,15 @@ def main():
     y_max = max(cropped_y) + y_margin
 
     plt.margins(x=0.05, y=0.05)
-    plt.xticks(np.arange(int(min(cropped_x) // 10 * 10), max(cropped_x) + 1, 40))
-    plt.yticks(np.arange(int(min(cropped_y) // 10 * 10), int(max(cropped_y)), 20))
-    #plt.grid(True)
-    #plt.xlim(min(x), max(x))
+    plt.xticks(np.arange(int(min(cropped_x) // 10 * 10),
+                         max(cropped_x) + 1, 40))
+    plt.yticks(np.arange(int(min(cropped_y) // 10 * 10),
+                         int(max(cropped_y)), 20))
     plt.ylim(y_min, y_max)
-    #plt.xticks(x)
 
-    # Graph printing
-    plt.legend() # ensures the labels are displayed.
-    
     # Show plot
     plt.show()
 
 
 if __name__ == "__main__":
-      main()
+    main()
